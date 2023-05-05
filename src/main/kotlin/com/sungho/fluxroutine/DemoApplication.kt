bfe6ae7e0f3759e47b905bf436992619171cabf8
sungho <tjdgh1231@tpirates.com>
2023-05-05 16:28:02 +0900

    feat : blockhound추가

--- src/main/kotlin/com/sungho/fluxroutine/DemoApplication.kt
+++ src/main/kotlin/com/sungho/fluxroutine/DemoApplication.kt
@@ -1,11 +1,24 @@
 package com.sungho.fluxroutine
 
+import kotlinx.coroutines.debug.CoroutinesBlockHoundIntegration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.blockhound.BlockHound
+import reactor.blockhound.integration.ReactorIntegration
+import reactor.blockhound.integration.RxJava2Integration
+import reactor.blockhound.integration.StandardOutputIntegration
+
 
 @SpringBootApplication
 class FluxRoutineApplication
 
 fun main(args: Array<String>) {
     runApplication<FluxRoutineApplication>(*args)
+    BlockHound
+        .builder()
+        .with(CoroutinesBlockHoundIntegration())
+        .with(StandardOutputIntegration())
+        .with(ReactorIntegration())
+        .with(RxJava2Integration())
+        .install()
 }
